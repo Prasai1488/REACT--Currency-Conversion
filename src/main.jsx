4423f6b5bcf7ed9a1590355a625ec60522732d02
Prasai1488 <prasaiprithvi4@gmail.com>
2024-07-02 18:05:32 +0545

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+import './index.css'
+import CurrencyDisplay from './components/CurrencyDisplay'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CurrencyDisplay />
+  </React.StrictMode>,
+)
